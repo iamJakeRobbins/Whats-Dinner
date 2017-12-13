935a1d49d5dd81e9e98d749c75c8216a90a5432f
Jake Robbins <jrobbinsd@gmail.com>
2017-12-12 18:45:43 -0700

    cleaned up some messy code

--- public/app.js
+++ public/app.js
@@ -1,33 +1,35 @@
 var form = document.getElementById('searchItem');
-form.addEventListener('submit', function(event){
-	document.getElementsByClassName('pure-menu pure-menu-scrollable custom-restricted')[0].innerHTML=''
-	event.preventDefault();
-	var id = event.target.elements.search.value;
-	fetch('https://galvanize-cors-proxy.herokuapp.com/http://food2fork.com/api/search?key=8dc9930929d68ece9d0e39c41c097980&q='+ id)
-	.then (function(id){
-		return id.json()
-		.then (function(recipeData){
-			food(recipeData)
-			console.log(recipeData);
-		})
-	})
+
+form.addEventListener('submit', function(event) {
+  document.getElementsByClassName('pure-menu pure-menu-scrollable custom-restricted')[0].innerHTML = ''
+  event.preventDefault();
+  var id = event.target.elements.search.value;
+  fetch('https://galvanize-cors-proxy.herokuapp.com/http://food2fork.com/api/search?key=8dc9930929d68ece9d0e39c41c097980&q=' + id)
+    .then(function(id) {
+      return id.json()
+        .then(function(recipeData) {
+          food(recipeData)
+        })
+    })
 })
-function food(recipeData){
-	for (i=0; i<recipeData.recipes.length; i++){
-		matador(recipeData.recipes[i])
-	}
+
+function food(data) {
+  for (i = 0; i < data.recipes.length; i++) {
+    recipeDisplay(data.recipes[i])
+  }
 }
-function matador(nubs){
-	var div = document.createElement('div')
-	var h2 = document.createElement('h2')
-	var anchor = document.createElement('a')
-	var img = document.createElement('img')
-	var visualizer = document.getElementsByClassName('pure-menu pure-menu-scrollable custom-restricted')[0]
-		h2.innerText = nubs.title
-		img.setAttribute('src',nubs.image_url)
-		anchor.setAttribute('href', nubs.source_url)
-		anchor.setAttribute('target', 'blank')
-		anchor.append(img)
-		div.append(h2, anchor)
-		visualizer.append(div)
+
+function recipeDisplay(recipe) {
+  var div = document.createElement('div')
+  var h2 = document.createElement('h2')
+  var anchor = document.createElement('a')
+  var img = document.createElement('img')
+  var visualizer = document.getElementsByClassName('pure-menu pure-menu-scrollable custom-restricted')[0]
+  h2.innerText = recipe.title
+  img.setAttribute('src', recipe.image_url)
+  anchor.setAttribute('href', recipe.source_url)
+  anchor.setAttribute('target', 'blank')
+  anchor.append(img)
+  div.append(h2, anchor)
+  visualizer.append(div)
 }
